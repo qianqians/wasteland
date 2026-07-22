b245d83dbc926eacdb042d9d1656d8d5e5a96d65
吴铭 <451517996@qq.com>
2026-07-23 00:00:55 +0800

    服务器引擎变更

--- server/lgbf/underlying/codegen.bat
+++
@@ -1,8 +0,0 @@
-cd ../../tools
-
-protoc --csharp_out=../lgbf/hub  --proto_path=../lgbf/underlying  ../lgbf/underlying/underlying.proto
-protoc --csharp_out=../gem/unity/Assets/Script/ServerSDK  --proto_path=../lgbf/underlying  ../lgbf/underlying/underlying.proto
-
-protoc --plugin=../../node_modules/.bin/protoc-gen-ts_proto \
-    --ts_proto_out=../gem/ccc/assets/script/ServerSDK \
-    --proto_path=../lgbf/underlying  ../lgbf/underlying/underlying.proto
--- server/lgbf/underlying/codegen.sh
+++
@@ -1,6 +0,0 @@
-protoc --csharp_out=../hub  --proto_path=../underlying  ../underlying/underlying.proto
-protoc --csharp_out=../../gem/unity/Assets/Script/ServerSDK  --proto_path=../underlying  ../underlying/underlying.proto
-
-protoc --plugin=../../node_modules/.bin/protoc-gen-ts_proto \
-    --ts_proto_out=../../gem/ccc/assets/script/ServerSDK \
-    --proto_path=../underlying  ../underlying/underlying.proto
