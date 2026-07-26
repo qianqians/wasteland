b6cd6c25f9551dd4689527027972bce9950c1e4c
吴铭 <451517996@qq.com>
2026-07-26 23:37:04 +0800

    地图1

---
+++ tiledMap_怪物森林/1024X1024.tsx
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="1024X1024" tilewidth="64" tileheight="64" tilecount="320" columns="16">
+ <image source="images/1024X1024.png" width="1024" height="1280"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="224" duration="800"/>
+   <frame tileid="229" duration="800"/>
+   <frame tileid="224" duration="800"/>
+  </animation>
+ </tile>
+ <tile id="150">
+  <animation>
+   <frame tileid="150" duration="400"/>
+   <frame tileid="151" duration="400"/>
+   <frame tileid="152" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="166">
+  <animation>
+   <frame tileid="166" duration="600"/>
+   <frame tileid="183" duration="600"/>
+   <frame tileid="166" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="167">
+  <animation>
+   <frame tileid="167" duration="600"/>
+   <frame tileid="184" duration="600"/>
+   <frame tileid="167" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="208">
+  <animation>
+   <frame tileid="208" duration="800"/>
+   <frame tileid="213" duration="600"/>
+   <frame tileid="261" duration="800"/>
+   <frame tileid="208" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="210">
+  <animation>
+   <frame tileid="210" duration="200"/>
+   <frame tileid="209" duration="200"/>
+   <frame tileid="211" duration="200"/>
+   <frame tileid="212" duration="200"/>
+   <frame tileid="210" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="224">
+  <animation>
+   <frame tileid="224" duration="800"/>
+   <frame tileid="229" duration="600"/>
+   <frame tileid="277" duration="800"/>
+   <frame tileid="224" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="225">
+  <animation>
+   <frame tileid="225" duration="400"/>
+   <frame tileid="226" duration="400"/>
+   <frame tileid="227" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="240">
+  <animation>
+   <frame tileid="240" duration="800"/>
+   <frame tileid="245" duration="600"/>
+   <frame tileid="293" duration="800"/>
+   <frame tileid="240" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="241">
+  <animation>
+   <frame tileid="241" duration="400"/>
+   <frame tileid="242" duration="400"/>
+   <frame tileid="243" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="256">
+  <animation>
+   <frame tileid="256" duration="400"/>
+   <frame tileid="272" duration="200"/>
+   <frame tileid="288" duration="600"/>
+   <frame tileid="304" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="257">
+  <animation>
+   <frame tileid="257" duration="400"/>
+   <frame tileid="258" duration="400"/>
+   <frame tileid="259" duration="400"/>
+   <frame tileid="260" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="289">
+  <animation>
+   <frame tileid="289" duration="200"/>
+   <frame tileid="290" duration="400"/>
+   <frame tileid="291" duration="400"/>
+   <frame tileid="289" duration="800"/>
+  </animation>
+ </tile>
+ <tile id="305">
+  <animation>
+   <frame tileid="305" duration="200"/>
+   <frame tileid="306" duration="400"/>
+   <frame tileid="307" duration="200"/>
+   <frame tileid="308" duration="400"/>
+   <frame tileid="309" duration="200"/>
+   <frame tileid="305" duration="600"/>
+  </animation>
+ </tile>
+ <tile id="306">
+  <animation>
+   <frame tileid="305" duration="400"/>
+   <frame tileid="307" duration="400"/>
+   <frame tileid="309" duration="400"/>
+   <frame tileid="308" duration="400"/>
+   <frame tileid="309" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="309">
+  <animation>
+   <frame tileid="309" duration="400"/>
+   <frame tileid="308" duration="400"/>
+   <frame tileid="306" duration="400"/>
+   <frame tileid="305" duration="400"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ tiledMap_怪物森林/bigTree.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="bigTree" tilewidth="1100" tileheight="1280" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="images/bigTree.png" width="1100" height="1280"/>
+ </tile>
+</tileset>
