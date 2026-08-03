e8b7dd98c0a6fe2e756b0a1c69ec10d124e78610
wuming <wuming@lumosplay.com>
2026-08-03 09:46:46 +0800

    lfs .dll

---
+++ art/tiledMap_天空之陆/map_ice.tsx
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="map_skyland" tilewidth="64" tileheight="64" tilecount="480" columns="24">
+ <image source="images/map_skyland.png" width="1536" height="1280"/>
+ <tile id="102">
+  <animation>
+   <frame tileid="102" duration="400"/>
+   <frame tileid="126" duration="400"/>
+   <frame tileid="102" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="103">
+  <animation>
+   <frame tileid="103" duration="400"/>
+   <frame tileid="127" duration="400"/>
+   <frame tileid="103" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="104">
+  <animation>
+   <frame tileid="104" duration="400"/>
+   <frame tileid="128" duration="400"/>
+   <frame tileid="104" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="105">
+  <animation>
+   <frame tileid="105" duration="400"/>
+   <frame tileid="129" duration="400"/>
+   <frame tileid="105" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="384">
+  <animation>
+   <frame tileid="384" duration="300"/>
+   <frame tileid="385" duration="300"/>
+   <frame tileid="386" duration="300"/>
+   <frame tileid="387" duration="300"/>
+   <frame tileid="388" duration="300"/>
+   <frame tileid="389" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="463">
+  <animation>
+   <frame tileid="463" duration="300"/>
+   <frame tileid="464" duration="300"/>
+   <frame tileid="465" duration="300"/>
+   <frame tileid="466" duration="300"/>
+   <frame tileid="463" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
