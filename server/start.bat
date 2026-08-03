86983b4cfc008ceba8cd7a3f8a1a5097ba66c78b
wuming <wuming@lumosplay.com>
2026-08-03 09:26:28 +0800

    脚本和忽略文件

--- server/start.bat
+++ server/start.bat
@@ -1,4 +1,4 @@
-cd ../../server/dependences/consul/
+cd ./dependences/windows/consul/
 start consul.exe agent -dev
 
 cd ../redis/
@@ -6,18 +6,11 @@ start start.bat
 
 timeout /t 3
 
-cd ../../../sample/server/bin
-start dbproxy.exe ../config/dbproxy.cfg
-start gate.exe ../config/gate.cfg
+cd ../../../bin/windows/
+start dbproxy.exe ../../config/dbproxy.cfg
+start gate.exe ../../config/gate.cfg
 
 timeout /t 3
-
-cd ../src
-start python rank_app.py ../config/rank.cfg
-
-timeout /t 3
-
-start python app.py ../config/player.cfg
 
 cd ../
 pause
