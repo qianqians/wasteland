bd3ed595ec326a45c92146131944ec16391eaa46
wuming <wuming@lumosplay.com>
2026-08-10 20:03:27 +0800

    fixed

--- wasteland/assets/script/ServerSDK/engine/player_ntf_client_cli.ts
+++ wasteland/assets/script/ServerSDK/engine/player_ntf_client_cli.ts
@@ -16,8 +16,8 @@ export class player_ntf_client_module {
         this.entity.reg_hub_notify_callback("task", this.task)
     }
 
-    public drop(hub_name:string, bin:Uint8Array):
-        inArray = decode(bin) as any;
+    public drop(hub_name:string, bin:Uint8Array) {
+        let inArray = decode(bin) as any;
         let _task_id = inArray[0];
         let _pkg_item:Array<common.item> = [];
         for (let v_5441b1b9_4592_5e1c_94ba_124d9c6a6f94 of inArray[1]) {
@@ -27,14 +27,14 @@ export class player_ntf_client_module {
         for (let v_e22d0078_3b03_5fe2_a310_c3f6c3c23740 of inArray[2]) {
             _total_item.push(common.protcol_to_item(v_e22d0078_3b03_5fe2_a310_c3f6c3c23740))
         }
-        s = new engine.session(hub_name)
+        let s = new engine.session(hub_name)
         for (let fn of this.on_drop) {
             fn(s, _task_id, _pkg_item, _total_item);
         }
     }
 
-    public task(hub_name:string, bin:Uint8Array):
-        inArray = decode(bin) as any;
+    public task(hub_name:string, bin:Uint8Array) {
+        let inArray = decode(bin) as any;
         let _task_list:Array<common.task_info> = [];
         for (let v_944f018b_64ee_5e22_a445_39fa78b68397 of inArray[0]) {
             _task_list.push(common.protcol_to_task_info(v_944f018b_64ee_5e22_a445_39fa78b68397))
@@ -43,7 +43,7 @@ export class player_ntf_client_module {
         for (let v_553ebab7_911a_59be_9a8f_20354c69a863 of inArray[1]) {
             _progress_list.push(common.protcol_to_task_progress_info(v_553ebab7_911a_59be_9a8f_20354c69a863))
         }
-        s = new engine.session(hub_name)
+        let s = new engine.session(hub_name)
         for (let fn of this.on_task) {
             fn(s, _task_list, _progress_list);
         }
