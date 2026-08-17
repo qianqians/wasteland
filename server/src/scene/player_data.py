62425d84515dc290a8ecff807ba34201a00fa1fb
wuming <wuming@lumosplay.com>
2026-08-17 18:50:30 +0800

    scene(_scene)

--- server/src/scene/player_data.py
+++ server/src/scene/player_data.py
@@ -67,7 +67,7 @@ class player_data(save, player):
     
     def entry_scene(self, _scene:any):
         from .scene import scene
-        self.scene:scene = _scene
+        self.scene:scene = scene(_scene)
         
     def begin_move(self, vertical_dir:direction, pos:position):
         (is_spawn, spawn_scene_name) = self.scene_data.begin_move(vertical_dir, pos)
